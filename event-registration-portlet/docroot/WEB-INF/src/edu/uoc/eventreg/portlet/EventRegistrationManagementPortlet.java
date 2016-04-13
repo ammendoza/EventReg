58e605f6d95de89c3cc3da67c22373ffb722f07f
Ana Mendoza <anamendoza.ibz@gmail.com>
2016-04-13 20:29:22 +0200

    Portlet Event Registration Management creado y service.xml completado.

---
+++ event-registration-portlet/docroot/WEB-INF/src/edu/uoc/eventreg/portlet/EventRegistrationManagementPortlet.java
@@ -0,0 +1,11 @@
+package edu.uoc.eventreg.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class EventRegistrationManagementPortlet
+ */
+public class EventRegistrationManagementPortlet extends MVCPortlet {
+ 
+
+}
